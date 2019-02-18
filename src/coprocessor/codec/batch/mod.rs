ae63cfa4dc23fe590825a06443c04b3a8e93b2f7
Shirly <AndreMouche@126.com>
2019-02-18 11:25:50 +0800

    Revert "Extract LazyBatchColumn (#4211)"
    This reverts commit 522b0666f546afa8f4eab4f9404f6607eae5af18.

--- src/coprocessor/codec/batch/mod.rs
+++ src/coprocessor/codec/batch/mod.rs
@@ -11,10 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod lazy_column;
 mod rows;
 mod vector;
 
-pub use self::lazy_column::LazyBatchColumn;
 pub use self::rows::BatchRows;
 pub use self::vector::VectorValue;
